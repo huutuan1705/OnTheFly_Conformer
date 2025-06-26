37473c7e40379accff10d05880ee1507b11de6b0
tuan.nh <tuan.nh@tinasoft.com.vn>
2025-06-26 16:48:24 +0700

    update main

--- src/conformer.py
+++ src/conformer.py
@@ -202,6 +202,7 @@ class ConformerBlock(nn.Module):
         self.post_norm = nn.LayerNorm(dim)
 
     def forward(self, x, mask = None):
+        x = self.ff1(x) + x
         x = self.attn(x, mask = mask) + x
         x = self.conv(x) + x
         x = self.ff2(x) + x
