0d84336670985c00475902b8604720ca9e99a069
tuan.nh <tuan.nh@tinasoft.com.vn>
2025-06-26 14:40:39 +0700

    update backbone

--- src/model.py
+++ src/model.py
@@ -13,17 +13,15 @@ class FG_SBIR(nn.Module):
         super(FG_SBIR, self).__init__()
         self.args = args
         self.sample_embedding_network = InceptionV3(args=args).to(device)
-        # self.attention = Conformer(dim=2048).to(device)
         self.attention = SelfAttention(args).to(device)
         self.linear = Linear_global(feature_num=self.args.output_size).to(device)
         
         self.sketch_embedding_network = InceptionV3(args=args).to(device)
-        # self.sketch_attention = Conformer(dim=2048).to(device)
         self.sketch_attention = SelfAttention(args).to(device)
         self.sketch_linear = Linear_global(feature_num=self.args.output_size).to(device)
     
         def init_weights(m):
-            if type(m) == nn.Linear or type(m) == nn.Conv1d or type(m) == nn.Parameter:
+            if type(m) == nn.Linear or type(m) == nn.Conv2d or type(m) == nn.Parameter:
                 nn.init.kaiming_normal_(m.weight)
                 
         if self.args.use_kaiming_init:
@@ -33,11 +31,11 @@ class FG_SBIR(nn.Module):
             self.sketch_attention.apply(init_weights)
             self.sketch_linear.apply(init_weights)
             
-    def forward(self, batch):        
+    def forward(self, batch):
         sketch_img = batch['sketch_img'].to(device)
         positive_img = batch['positive_img'].to(device)
         negative_img = batch['negative_img'].to(device)
-            
+        
         positive_feature = self.sample_embedding_network(positive_img)
         negative_feature = self.sample_embedding_network(negative_img)
         sketch_feature = self.sketch_embedding_network(sketch_img)
@@ -49,21 +47,19 @@ class FG_SBIR(nn.Module):
         positive_feature = self.linear(positive_feature)
         negative_feature = self.linear(negative_feature)
         sketch_feature = self.sketch_linear(sketch_feature)
-                
-        return positive_feature, negative_feature, sketch_feature
+        
+        return sketch_feature, positive_feature, negative_feature
     
-    def test_forward(self, batch):        
-        sketch_img = batch['sketch_img'].to(device)
-        positive_img = batch['positive_img'].to(device)
-            
-        positive_feature = self.sample_embedding_network(positive_img)
+    def test_forward(self, batch):
+        sketch_feature = self.sketch_embedding_network(batch['sketch_img'].to(device))
+        positive_feature = self.sample_embedding_network(batch['positive_img'].to(device))
+        
         positive_feature = self.attention(positive_feature)
-        positive_feature = self.linear(positive_feature)
-        
-        sketch_feature = self.sketch_embedding_network(sketch_img)
         sketch_feature = self.sketch_attention(sketch_feature)
-        sketch_feature = self.sketch_linear(sketch_feature)
         
+        positive_feature = self.linear(positive_feature)
+        sketch_feature = self.sketch_linear(sketch_feature)
+            
         return sketch_feature, positive_feature
     
     
