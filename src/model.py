b637cdd0698065e97516b3022b7b449bf96cd4cf
tuan.nh <tuan.nh@tinasoft.com.vn>
2025-06-26 09:28:53 +0700

    update model

--- src/model.py
+++ src/model.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 
 from src.backbones import InceptionV3
 from src.attention import Linear_global, SelfAttention
-from src.vision_conformer import VisionConformer
+from src.conformer import Conformer
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -13,10 +13,12 @@ class FG_SBIR(nn.Module):
         super(FG_SBIR, self).__init__()
         self.args = args
         self.sample_embedding_network = InceptionV3(args=args).to(device)
-        self.attention = SelfAttention(args).to(device)
+        self.attention = Conformer(args).to(device)
         self.linear = Linear_global(feature_num=self.args.output_size).to(device)
         
-        self.conformer = VisionConformer().to(device)
+        self.sketch_embedding_network = InceptionV3(args=args).to(device)
+        self.sketch_attention = Conformer(args).to(device)
+        self.sketch_linear = Linear_global(feature_num=self.args.output_size).to(device)
     
         def init_weights(m):
             if type(m) == nn.Linear or type(m) == nn.Conv2d or type(m) == nn.Parameter:
